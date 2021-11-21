b65d9c9b54dcd768a689cd3ab05ea183d0dcab59
svaba <stefanivanovic1012@gmail.com>
2021-11-21 10:22:43 +0100

    A20 line enabled

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -69,7 +69,13 @@ load32:
     mov gs, ax
     mov ss, ax
     mov ebp, 0x00200000
-    mov esp, ebp
+    mov esp, ebp 
+
+    ;A20 line enabled
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
     jmp $
 
 
